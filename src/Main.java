abcbbad7892c2833b4caccf1df38c5cd6b8bb483
Нина Кесарева <n1n@list.ru>
2022-05-30 20:02:08 +0300

    task2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+        public static void main(String[] args) {
+            int costTicket = 8828;
+            int mile = 20;
+            int bonus = (costTicket / mile);
+            System.out.println(" Цена билета - " + costTicket + " р. ");
+            System.out.println(" Количество рублей для одной бонусной мили - " + mile + " р. ");
+            System.out.println(" Начислено бонусов - " + bonus + " р. ");
+        }
+
+
+    }
+
